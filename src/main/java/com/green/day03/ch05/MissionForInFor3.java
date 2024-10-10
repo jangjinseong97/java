ee75aafe168270aaee9be0fa1137a4f8f5cf17ec
jangjinseong <hn04223@gmail.com>
2024-10-10 17:56:32 +0900

    241010

---
+++ src/main/java/com/green/day03/ch05/MissionForInFor3.java
@@ -0,0 +1,31 @@
+package com.green.day03.ch05;
+
+public class MissionForInFor3 {
+    public static void main(String[] args) {
+        int star = (int)((Math.random()) * 6.0) + 3;
+        for(int i=star ;i>0 ;i--){
+            for(int k=star; k>0; k--){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        System.out.println("----");
+        for(int i=1 ;i<=star ;i++){
+            for(int k=1;k<=star;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        // 항상 for문안이 true가 되야 실행이 된다는것을 생각하기 자꾸 항상 false인 상황을 만들때가 많음
+        System.out.println(star);
+        for(int i=1; i<=star*star; i++){
+            System.out.print("*");
+            if(i % star == 0){
+                System.out.println();
+            }
+        }
+        // 위와 같이 for문 하나로도 해결 가능
+    }
+}
