48f5b97309ee7774e8aad391e13a14ad663d6e53
jangjinseong <hn04223@gmail.com>
2024-10-10 14:19:16 +0900

    241010

---
+++ src/main/java/com/green/etc/ForFor.java
@@ -0,0 +1,13 @@
+package com.green.etc;
+
+public class ForFor {
+    public static void main(String[] args) {
+        // 구구단 5단 출력하는 프로그렘 for문으로
+        for(int i=1; i<10; i++){
+            for(int j=1; j<10; j++) {
+                System.out.printf("%d x %d = %d\n", i, j, (j * i));
+            }
+            System.out.println("----------");
+        }
+    }
+}
