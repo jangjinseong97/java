f3465ef301630051b611df0a6b16c9680a31b0fe
jangjinseong <hn04223@gmail.com>
2024-10-10 17:58:27 +0900

    241010

--- src/main/java/com/green/day03/ch05/MissionForInFor3.java
+++ src/main/java/com/green/day03/ch05/MissionForInFor3.java
@@ -20,12 +20,13 @@ public class MissionForInFor3 {
         }
         // 항상 for문안이 true가 되야 실행이 된다는것을 생각하기 자꾸 항상 false인 상황을 만들때가 많음
         System.out.println(star);
-        for(int i=1; i<=star*star; i++){
+        for(int i=1; i<=(star*star); i++){
             System.out.print("*");
             if(i % star == 0){
                 System.out.println();
             }
         }
         // 위와 같이 for문 하나로도 해결 가능
+        // star*star 바로 넣는것도 괜찮지만 변수 선언을 외부에서 한 뒤 끌어오는것이 더 안전
     }
 }
